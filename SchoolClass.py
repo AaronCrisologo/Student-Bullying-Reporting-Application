a79c95148c93458bd968d7942bf1c238239e9d12
Louis <126314671+leaurix@users.noreply.github.com>
2025-03-22 15:20:23 +0800

    Update SchoolClass.py
    School instance Fix

--- SchoolClass.py
+++ SchoolClass.py
@@ -1,4 +1,19 @@
 class School:
+    _instance = None  
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(School, cls).__new__(cls)
+            cls._instance.reports = []
+            cls._instance.users = []
+        return cls._instance
+    
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls()
+        return cls._instance if cls._instance else cls()
+        
     def __init__(self, schoolID: str, name: str, address: str):
         self.schoolID = schoolID
         self.name = name
