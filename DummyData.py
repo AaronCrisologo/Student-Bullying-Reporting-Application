9a7cdc530d41ace509e2c43aad4be7b6f47e883c
Louis <126314671+leaurix@users.noreply.github.com>
2025-03-22 10:43:14 +0800

    Update DummyData.py
    Added Student Dummy Reports

--- DummyData.py
+++ DummyData.py
@@ -10,6 +10,8 @@ school = School(schoolID="SCH001", name="BatStateU-The-NEU", address="Golden Cou
 teacher1 = Teacher("T001", "Raffy Tulfo", "Raffy@teacher.com", passwordHash=hash_password("Raffy Tulfo in Action"))
 teacher2 = Teacher("T002", "Erwin Tulfo", "Erwin@teacher.com", passwordHash=hash_password("Para sa mahirap"))
 admin1 = Administrator("A001", "Cardo Dalisay", "Cardo@admin.com", passwordHash=hash_password("bengbeng"))
+student1 = Student("S001", "Ben Gonzales", "benG@student.com", grade=10, passwordHash=hash_password("Bengbeng"))
+student2 = Student("S002", "Jose Rizz Al", "JoseA@student.com", grade=11, passwordHash=hash_password("NoliMeTangereLite"))
 school.users.extend([teacher1, teacher2, admin1])
 
 # Dummy Reports
